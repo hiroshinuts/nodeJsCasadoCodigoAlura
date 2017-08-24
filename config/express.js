a71930ecf91e45efa52f10e55df04ac199bd6ce9
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2017-08-24 15:09:30 -0300

    FIM CAP 8- WEBSOCKET

--- config/express.js
+++ config/express.js
@@ -7,9 +7,11 @@ module.exports = function() {
 
 	var app = express();
 	
+	app.use(express.static('./app/public'));
 	app.set('view engine', 'ejs');
 	app.set('views','./app/views');
 
+	
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
 	app.use(expressValidator());
